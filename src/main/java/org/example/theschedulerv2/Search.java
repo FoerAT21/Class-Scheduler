505f6ebb29443723ae1ce99b4dfd4a1860ecea31
WEAVERDM21 <weaverdm21@gcc.edu>
2024-04-20 19:48:35 -0400

    Finally able to connect to SQLite database and can make queries. Still working on searching by time and also formatting output.

--- src/main/java/org/example/theschedulerv2/Search.java
+++ src/main/java/org/example/theschedulerv2/Search.java
@@ -1,33 +1,68 @@
 package org.example.theschedulerv2;
 import java.sql.*;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Search {
     public static void main(String[] args) {
-        search("COMP", 424,'c', "", 0, 'c', 'c', ' ', 'c', 'c', "", "", "", "");
+        search("COMP", null, null, null, null, null, "9:00 AM", null,null);
     }
 
-    public static ArrayList<Class> search(String Department, int CourseNumber, char Section, String CourseName, int NumCredits, char M, char T, char W, char R, char F, String StartTime, String EndTime, String InstructorLast, String InstructorFirst){
+    private static Connection connect() {
+        // SQLite's connection string
+        String url = "jdbc:sqlite:C://sqlite/database.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Successfully connected to database!");
+        return conn;
+    }
+
+    private static String formatQuery(String Department, String CourseNumber, String Section, String CourseName, String NumCredits, String DaysOfWeek, String StartTime, String EndTime, String InstructorName){
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT * FROM course WHERE");
+        if (Department != null){query.append(" /department = '" + Department + "'");}
+        if (CourseNumber != null){query.append(" /number = '" + CourseNumber + "'");}
+        if (Section != null){query.append(" /section = '" + Section + "'");}
+        if (CourseName != null){query.append(" /name LIKE '%" + CourseName + "%'");}
+        if (NumCredits != null){query.append(" /hours = '" + NumCredits + "'");}
+        if (DaysOfWeek != null){query.append(" /weekday = '" + DaysOfWeek + "'");}
+        if (StartTime != null){query.append(" /startTime >= '" + StartTime + "'");}
+        if (EndTime != null){query.append(" /TIME(endTime) <= TIME(" + EndTime + ")");}
+        if (InstructorName != null){query.append(" /professorName LIKE  '%" + InstructorName + "%'");}
+        String q = query.toString();
+        return q.replaceFirst("/", "").replaceAll("/", "AND ");
+    }
+
+    public static ArrayList<Class> search(String Department, String CourseNumber, String Section, String CourseName, String NumCredits, String DaysOfWeek, String StartTime, String EndTime, String InstructorName){
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
 
         try {
             // Establishing the connection
-            conn = DriverManager.getConnection("jdbc:mysql:src/Database/database.sql");
+            conn = connect();
 
             // Creating a statement
             stmt = conn.createStatement();
 
             // Executing a query
-            rs = stmt.executeQuery("SELECT * FROM your_table WHERE Department = 'COMP'");
+            String query = formatQuery(Department, CourseNumber, Section, CourseName, NumCredits, DaysOfWeek, StartTime, EndTime, InstructorName);
+            System.out.println(query);
+            rs = stmt.executeQuery(query);
 
             // Processing the results
             while (rs.next()) {
                 // Retrieve data from the result set
-                String department = rs.getString("Department");
-                int courseNumber = rs.getInt("CourseNumber");
-                String section = rs.getString("Section");
+                String department = rs.getString("department");
+                int courseNumber = rs.getInt("number");
+                String section = rs.getString("section");
                 // Process the data
                 System.out.println("Dep: " + department + ", CourseNum: " + courseNumber + "Section" + section);
             }
